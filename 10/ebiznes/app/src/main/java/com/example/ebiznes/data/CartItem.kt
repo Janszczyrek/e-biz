633507503398d3c1b9ec4cb966fabe562f451890
Janszczyrek <janwalus@gmail.com>
2025-06-01 16:56:53 +0200

    object design exercise added by mistake

--- 10/ebiznes/app/src/main/java/com/example/ebiznes/data/CartItem.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ebiznes.data
-
-data class CartItem(
-    val product: Product,
-    var quantity: Int
-)
--- 10/ebiznes/app/src/main/java/com/example/ebiznes/data/Product.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ebiznes.data
-
-data class Product(
-    val name: String,
-    val price: Double,
-    val description: String,
-    val category: String
-)
